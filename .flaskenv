eeed4a79961d0196dea6d2809e46aefb77e6e469
Andrew Lim <Andrew.Lim@e14.io>
2025-01-20 17:52:01 -0300

    Added forms.py

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,3 @@
-FLASK_APP=app/microblog.py  # Path to the main Flask application file
+FLASK_APP=microblog.py  # Path to the main Flask application file
 FLASK_ENV=development       # Enables debug mode (optional for development)
 FLASK_DEBUG=1               # Enables the debugger (optional)
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,8 @@
 from flask import render_template
 from app import app
-from app import LoginForm
 
 @app.route('/')
 @app.route('/index')
-@app.route('/login')
-def login():
-    form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
 def index():
     user = {'username': 'Lim'}
     posts = [
@@ -15,9 +10,11 @@ def index():
             'author': {'username': 'John'},
             'body': 'Beautiful day in Portland!'
         },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    from app.forms import LoginForm  # Move the import here
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
