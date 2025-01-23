3ac732bff7d8a97419061c81591198e263566604
Andrew Lim <Andrew.Lim@e14.io>
2025-01-23 17:55:59 -0300

    Added user_loader function and werkzeug password hasher and checker functions

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,7 @@
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
 from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
@@ -17,9 +21,5 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
