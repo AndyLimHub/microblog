2f5ba68ba0e1fbe40582bffcad73843ae2143ee9
Andrew Lim <133227688+AndyLimHub@users.noreply.github.com>
2025-01-24 17:56:50 -0300

    Added different data validators to my login form

--- app/models.py
+++ app/models.py
@@ -9,11 +9,9 @@ from app import login
 
 @login.user_loader
 def load_user(id):
-    return db.sessions.get(User, int(id))
+    return db.session.get(User, int(id))
 
-
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
@@ -28,13 +26,13 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
-    
+        
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     body: so.Mapped[str] = so.mapped_column(sa.String(140))
     timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('user.id'), index=True)
     author: so.Mapped['User'] = so.relationship('User', back_populates='posts')
-
+    
     def __repr__(self):
         return '<Post {}>'.format(self.body)
--- microblog.py
+++ microblog.py
@@ -1,8 +1,9 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import app,db
+from werkzeug.security import generate_password_hash, check_password_hash
 from app.models import User, Post
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post, 'generate_password_hash': generate_password_hash, 'check_password_hash': check_password_hash, 'User': User}
